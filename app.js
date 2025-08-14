ac6daa368040cd6e60e54fdac4d92eded3575411
Vignesh8500 <vaddevignesh123@gmail.com>
2025-08-14 15:18:10 +0530

    prepare backend for railway deploy2

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ app.listen(8080,()=>{
     console.log("Listening from Backend");
 });
 const connection = mysql.createConnection({
-    host: process.env.SQL_HOST,
-    user: process.env.SQL_USER,
-    database: process.env.SQL_DATABASE,
-    password: process.env.SQL_PASSWORD,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    database: process.env.MYSQLDATABASE,
+    password: process.env.MYSQLPASSWORD,
+    port:MYSQLPORT,
 });
 connection.connect((err)=>{
     if(err){

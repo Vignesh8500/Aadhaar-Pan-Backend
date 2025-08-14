923280c095cb1323846208224ea9236e5a1fa72c
Vignesh8500 <vaddevignesh123@gmail.com>
2025-08-14 14:30:51 +0530

    prepare backend for railway

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.listen(8080,()=>{
     console.log("Listening from Backend");
 });
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "aadhar_pan_data",
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    database: process.env.SQL_DATABASE,
     password: process.env.SQL_PASSWORD,
 });
 connection.connect((err)=>{
